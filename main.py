e75ce500084ba11eee06b4d900042e1d577f328c
Ghafoor <ghafoornazari@proton.me>
2025-02-26 00:23:55 +0100

    Wrote the key generation fun

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import cryptography
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+import os
 
 def main():
     pass
@@ -8,6 +11,21 @@ def encrypt():
 
 def decrypt():
     pass
+
+def generate_key(password: str) -> bytes:
+    password_bytes = password.encode()
+    salt = os.urandom(16)
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+
+    key = kdf.derive(password_bytes)
+    return key, salt
 
 if __name__ == "__main__":
     main()
